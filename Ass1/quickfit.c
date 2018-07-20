deb13f26a683680bd6a3eeb4c76888c8b72f5a7f
Jesse E <ericksenjesse@gmail.com>
2018-07-20 14:44:28 -0700

    S

--- Ass1/quickfit.c
+++ Ass1/quickfit.c
@@ -314,7 +314,8 @@ static void do_free (void *ptr) {
         if(quick_list[indexOf] == NULL){
             block->data.next = NULL;
             quick_list[indexOf] = block;
-            printf("Initialized quick_list for %d size \n", (block->data.size + 1));
+             unsigned long curr_addr = (unsigned long)block;
+            printf("Initialized quick_list for %d size. Block address: %x\n", (block->data.size + 1), curr_addr);
         } else {
             printf("Adding block to quick_list: %d\n", block->data.size);
             curr = quick_list[indexOf];
