3c85aecae728ac36e8b84f5aca713dbca0b0a125
Jesse E <ericksenjesse@gmail.com>
2018-07-22 18:48:00 -0700

    revised

---
+++ Ass1/include/heap347.h
@@ -0,0 +1,19 @@
+/*  heap347.h
+
+	Header File for heap management routines
+	CSCI 347, Assignment 1
+	Summer 2018
+
+	Jesse Ericksen, W01173602 
+
+*/
+
+
+#ifndef HEAP_347
+#define HEAP_347
+
+void *malloc347(int size);
+void free347(void *ptr);
+
+
+#endif
